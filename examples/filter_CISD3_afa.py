282ff5692ad5bef463cdd556688a56c57efc0688
RaheelSyedAhmed <raheelsyedahmed@gmail.com>
2024-08-30 17:56:36 -0500

    Updated MSATools write example and documentation.

--- examples/filter_CISD3_afa.py
+++ examples/filter_CISD3_afa.py
@@ -3,11 +3,11 @@ from src.dcatoolkit.analytics import MSATools
 from src.dcatoolkit.representation import DirectInformationData, StructureInformation, Pairs, ResidueAlignment
 import numpy as np
 
-"""
+
 CISD3_MSA = MSATools.load_from_file("examples/files/output_MSA_CISD3")
 CISD3_filtered_MSA = MSATools(CISD3_MSA.filter_by_continuous_gaps(35))
 CISD3_filtered_MSA.write("examples/outputs/CISD3_filtered_35_MSA.fasta")
-"""
+
 
 struc_6avj = StructureInformation.fetch_pdb("6AVJ")
 print(struc_6avj.get_full_sequence('A'))
