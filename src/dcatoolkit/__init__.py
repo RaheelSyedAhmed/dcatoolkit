e63cad2a49ba5f97d2bb8c71f266e941ba206b61
RaheelSyedAhmed <raheelsyedahmed@gmail.com>
2024-09-30 04:00:54 -0500

    Added all variable to __init__
    Allowed for all imports to be used.

--- src/dcatoolkit/__init__.py
+++ src/dcatoolkit/__init__.py
@@ -1,4 +1,6 @@
 
 __version__ = "0.1.9"
-from .representation import Pairs, DirectInformationData, StructureInformation, ResidueAlignment
+from .representation import Pairs, DirectInformationData, StructureInformation, ResidueAlignment, MMCIFInformation, PDBInformation
 from .analytics import MSATools
+
+__all__ = ['Pairs', 'DirectInformationData', 'StructureInformation', 'ResidueAlignment', 'MMCIFInformation', 'PDBInformation', 'MSATools']
