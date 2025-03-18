8fb3e0e0fd95e2d5aa8f8e5418870d1ad6d2d9ff
RaheelSyedAhmed <raheelsyedahmed@gmail.com>
2025-03-18 13:38:57 -0500

    Version 0.2.1

--- src/dcatoolkit/__init__.py
+++ src/dcatoolkit/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = "0.1.9"
+__version__ = "0.2.1"
 from .representation import Pairs, DirectInformationData, StructureInformation, ResidueAlignment, MMCIFInformation, PDBInformation
 from .analytics import MSATools
 
